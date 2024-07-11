54eee632013b9b7163587c8f581f9d6d20752983
Miguel Macedo Ferreira <mmmiguel_macedo@outlook.com>
2024-07-10 21:16:16 -0300

    Exclusão de pastas duplicadas.

--- src/main/java/com/psoft/pitsa/PitsaApplication.java
+++ src/main/java/com/psoft/pitsa/PitsaApplication.java
@@ -1,7 +1,6 @@
-package com.psoft.pitsa.com.psoft.pitsa;
+package com.psoft.pitsa;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class PitsaApplication {
